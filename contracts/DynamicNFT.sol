e3b96938acbe19214086ba0dc1af161946f01b56
sharathguvvala <guvvalasharath@gmail.com>
2022-07-10 23:11:09 +0530

    import

--- contracts/DynamicNFT.sol
+++ contracts/DynamicNFT.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
 contract DynamicNFT {
     
 }
